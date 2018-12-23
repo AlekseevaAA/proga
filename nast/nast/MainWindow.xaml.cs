c0949c23394f2270df624402f152c10f9291a5e1
AlekseevaAA <46092634+AlekseevaAA@users.noreply.github.com>
2018-12-23 23:15:50 +0300

    Add files via upload

---
+++ nast/nast/MainWindow.xaml.cs
@@ -0,0 +1,81 @@
+﻿using System.Windows;
+
+namespace nast
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(first.Text) || string.IsNullOrEmpty(second.Text))
+                MessageBox.Show("Введите значения");
+
+            bool parse1 = int.TryParse(first.Text, out int a);
+            bool parse2 = int.TryParse(second.Text, out int b);
+            if (parse1 && parse2)
+            {
+                var ans = a + b;
+                MessageBox.Show(ans.ToString());
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(first.Text) || string.IsNullOrEmpty(second.Text))
+                MessageBox.Show("Введите значения");
+
+            bool parse1 = int.TryParse(first.Text, out int a);
+            bool parse2 = int.TryParse(second.Text, out int b);
+            if (parse1 && parse2)
+            {
+                var ans = a - b;
+                MessageBox.Show(ans.ToString());
+            }
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(first.Text) || string.IsNullOrEmpty(second.Text))
+                MessageBox.Show("Введите значения");
+
+            bool parse1 = int.TryParse(first.Text, out int a);
+            bool parse2 = int.TryParse(second.Text, out int b);
+
+            if (parse1 && parse2)
+            {
+                var ans = a * b;
+                MessageBox.Show(ans.ToString());
+            }
+        }
+
+       
+
+        private void Button_Click_4(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(first.Text) || string.IsNullOrEmpty(second.Text))
+                MessageBox.Show("Введите значения");
+
+            bool parse1 = float.TryParse(first.Text, out float a);
+            bool parse2 = float.TryParse(second.Text, out float b);
+
+            if (b == 0)
+            {
+                MessageBox.Show("Нельзя делить на 0");
+                return;
+            }
+
+            if (parse1 && parse2)
+            {
+                var ans = a / b;
+                MessageBox.Show(ans.ToString());
+            }
+        }
+    }
+}
